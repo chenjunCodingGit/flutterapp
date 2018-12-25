64d0eff901e78b7161f7e4eb98ffc33219ffa9c3
271676725 <271676725@qq.com>
2018-12-25 22:11:58 +0800

    shopcar

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 // import 'practice/container.dart';
-import 'practice/tutoraler.dart';
+// import 'practice/tutoraler.dart';
 // import 'practice/buttoner.dart';
+import 'practice/parentProducter.dart';
+import 'practice/producter.dart';
 
 void main() => runApp(new Myapp());
 
 class Myapp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
@@ -15,7 +16,14 @@ class Myapp extends StatelessWidget {
         primaryColor: Colors.grey[100],
         primaryColorBrightness: Brightness.light,
       ),
-      home: new TutorialHome(),
+      home: new ShoppingList(
+        products: <Product>[
+          new Product(name: 'Eggs'),
+          new Product(name: 'Flour'),
+          new Product(name: 'Flour'),
+          new Product(name: 'Chocolate chips'),
+        ],
+      ),
     );
   }
 }
